f502f968efa345138e4ea380ede94b52c06e5499
Adil Aman Mohammed <adil.mohammed@okstate.edu>
2024-05-31 00:26:03 -0400

    app completed

---
+++ Small number game #3/Small number game #3/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  Small number game #3
+//
+//  Created by Adil Aman Mohammed on 5/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    @State var Number = 0
+    @State var changemethod : Bool = true
+    var body: some View {
+        VStack {
+            if Number > 100
+            {
+                Text(String(Number)).font(.largeTitle).fontWeight(.bold).foregroundColor(Color.red)
+            }
+            else if Number < 100 && Number > 60
+            {
+                Text(String(Number)).font(.largeTitle).fontWeight(.bold).foregroundColor(Color.orange)
+            }
+            else if Number < 61 && Number > 30
+            {
+                Text(String(Number)).font(.largeTitle).fontWeight(.bold).foregroundColor(Color.yellow)
+            }
+            else if Number < 31 && Number > -1
+            {
+                Text(String(Number)).font(.largeTitle).fontWeight(.bold).foregroundColor(.green)
+            }
+            
+            
+            
+            Button {
+                hit()
+            }
+            label : {
+                Image("hitbutton").resizable().aspectRatio(contentMode: .fit).frame(width: 150).cornerRadius(15).shadow(radius: 3)
+            }.font(.subheadline)
+        }
+        .padding()
+        
+    }
+    
+    
+    
+    func hit()
+    {
+        if Number < 0
+        {
+            changemethod = true
+        }
+        else if Number > 100
+        {
+            changemethod = false
+        }
+        
+        if changemethod == true
+        {
+            Number += Int.random(in: 1...10)
+        }
+        else if changemethod == false
+        {
+            Number -= Int.random(in: 1...10)
+        }
+        
+    }
+    
+    
+    
+}
+
+#Preview {
+    ContentView()
+}
+
+
---
+++ Small number game #3/Small number game #3/Small_number_game__3App.swift
@@ -0,0 +1,17 @@
+//
+//  Small_number_game__3App.swift
+//  Small number game #3
+//
+//  Created by Adil Aman Mohammed on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Small_number_game__3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
